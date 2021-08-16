e91c5c320036414c52d061ef04673aca89dcf5be
Kedoky <mikhail-kozulin@mail.ru>
2021-08-16 12:54:53 +0300

    Wrote down the fields for the entity "Street"

--- src/main/java/com/example/demo/entity/Street.java
+++ src/main/java/com/example/demo/entity/Street.java
@@ -1,4 +1,36 @@
 package com.example.demo.entity;
 
+import org.springframework.lang.NonNull;
+
+import javax.persistence.*;
+
+@Entity
+@Table
 public class Street {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @NonNull
+    private String streetName;
+
+    public Street() { }
+
+    public Street(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public Long getId() { return id; }
+
+    public void setStreetName(String streetName){
+        this.streetName = streetName;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
 }
