0aba6fe7971936a6c3352963c203407a9e69ec92
Kedoky <mikhail-kozulin@mail.ru>
2021-08-16 12:56:20 +0300

    Wrote down the fields for the entity "Town"

--- src/main/java/com/example/demo/entity/Town.java
+++ src/main/java/com/example/demo/entity/Town.java
@@ -1,5 +1,35 @@
 package com.example.demo.entity;
 
-public class Town {
+import javax.persistence.*;
 
+@Entity
+@Table
+public class Town {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    private String townName;
+
+    public Town() { }
+
+    public Town(String townName) {
+        this.townName = townName;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setTownName(String townName){
+        this.townName = townName;
+    }
+
+    public String getTownName() {
+        return townName;
+    }
 }
