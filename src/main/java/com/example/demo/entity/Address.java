6ccfee6b02d6bf0f74e9a563e7dd00b256aec7c2
Kedoky <mikhail-kozulin@mail.ru>
2021-08-16 13:05:39 +0300

    Create entity file "Address"

---
+++ src/main/java/com/example/demo/entity/Address.java
@@ -0,0 +1,63 @@
+package com.example.demo.entity;
+
+import org.springframework.lang.NonNull;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class Address {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "id_streets__town")
+    private StreetsInTown streets_town;
+
+    @NonNull
+    private int numberHouse;
+
+    @NonNull
+    private int numberFlat;
+
+    public Address() { }
+
+    public Address(StreetsInTown streets_town, int numberHouse, int numberFlat) {
+        this.streets_town = streets_town;
+        this.numberHouse = numberHouse;
+        this.numberFlat = numberFlat;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setIdTownStreet(StreetsInTown streets_town){
+        this.streets_town = streets_town;
+    }
+
+    public StreetsInTown getIdTownStreet() {
+        return streets_town;
+    }
+
+    public void setNumberHouse(int numberHouse){
+        this.numberHouse = numberHouse;
+    }
+
+    public int getNumberHouse() {
+        return numberHouse;
+    }
+
+    public void setNumberFlat(int numberFlat){
+        this.numberFlat = numberFlat;
+    }
+
+    public int getNumberFlat() {
+        return numberFlat;
+    }
+}
